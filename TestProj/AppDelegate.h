e61ea78cb8139237ea708174cf5e29060fdc8a85
lijia <lijia@huajiao.tv>
2020-05-20 10:52:07 +0800

    init proj

---
+++ TestProj/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestProj
+//
+//  Created by lijia on 2020/5/20.
+//  Copyright © 2020 huajiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TestProj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProj
+//
+//  Created by lijia on 2020/5/20.
+//  Copyright © 2020 huajiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
